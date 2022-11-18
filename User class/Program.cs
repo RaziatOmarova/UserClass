9c3c44972e959ee4bd1f18c8b5aecc77e6a0f67f
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-11-18 19:56:17 +0300

    Добавьте файлы проекта.

---
+++ User class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>
+             {
+                 new User("Георгий", "Миронов",23),
+                 new User("Щльга", "Сорокина",16),
+                 new User("Михаил", "Соколов",17),
+                 new User("Софья", "Цветкова",20)
+             };
+            var age = users.Where(item => item.Age >= 18).ToList();
+            int n = 0;
+            foreach(var item in age)
+            {
+                n++;
+            }
+            Console.WriteLine($"Пользователи 18 лет и старше: {n}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ User class/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_class
+{
+    internal class User
+    {
+        public User(string firstname, string lastname, byte age)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Age = age;
+        }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public byte Age { get; set; }
+    }
+}
